1f635a41645132c0bfb7a2eb419b607018388bb4
IH-Tahim <imamhasan.01998@gmail.com>
2021-10-18 01:36:12 +0600

    firebase project setup and other setup

---
+++ src/Firebass/Firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBj8wYn7FlberfX37QPw3rLiN0-TtmkvU4",
+    authDomain: "firebass-prac-2.firebaseapp.com",
+    projectId: "firebass-prac-2",
+    storageBucket: "firebass-prac-2.appspot.com",
+    messagingSenderId: "683569241979",
+    appId: "1:683569241979:web:fff8ae82eebdfdf07a27de",
+    measurementId: "G-8PWQK73CNQ"
+};
+export default firebaseConfig;
---
+++ src/Firebass/Firebase.init.js
@@ -0,0 +1,9 @@
+import { initializeApp } from "firebase/app";
+import firebaseConfig from "./Firebase.config";
+
+
+// Initialize Firebase
+const initAuth = () => {
+    initializeApp(firebaseConfig);
+}
+export default initAuth;
